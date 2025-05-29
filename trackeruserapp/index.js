60221686e86d67876366e2122035fa70a5eb1610
samuelrodrigues <samuelrodriguessouzaiphone@gmail.com>
2025-05-29 01:49:53 -0300

    feat: middleware to handle users data

--- trackeruserapp/index.js
+++ trackeruserapp/index.js
@@ -2,14 +2,35 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser');
+const ObjectId = require('bson-objectid');
 
 app.use(cors())
+app.use(express.json());
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', (req, res, next) => {
+  const { username } = req.body ?? {}
 
+  if (!username) {
+    return res.json({ error: "No username provided" });
+  }
+
+  const id = new ObjectId().toHexString();
+  console.log("🚀 ~ app.post ~ ObjectId:", id)
+
+  res.json({
+    username: username,
+    _id: id
+  })
+
+  next()
+})
 
 
 
